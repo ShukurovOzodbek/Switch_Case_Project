ae9ca99c8909dd379dd13336c5007c85bae51a5f
ShukurovOzodbek <shukurov.ozodbek.jaloliddinovich@gmail.com>
2022-02-18 19:45:58 +0500

    Project

---
+++ script.js
@@ -0,0 +1,71 @@
+// 1 - Browser
+// let browser =  prompt('What is your browser name')
+// switch (browser) {
+//     case 'Edge':
+//       alert( "You've got the Edge!" );
+//       break;
+  
+//     case 'Chrome':
+//     case 'Firefox':
+//     case 'Safari':
+//     case 'Opera':
+//       alert( 'Okay we support these browsers too' );
+//       break;
+  
+//     default:
+//       alert( 'We hope that this page looks ok!' );
+// }
+
+// 2
+// let hello = confirm('Hello guy how are you') 
+// switch(hello) {
+//     case true:
+//         let prompts = prompt('What is your name')
+//         let begin = confirm(`Good ${prompts} our program calculates numbers`)
+//         if (begin === true) {
+//             let a = +prompt('Write first number')
+//             let operate = prompt('You sholud wrote one of this methods: +,  -,  *,  /,  ^,  %')
+//             let b = +prompt('Write second number')
+//             switch (operate) {
+//                 case "+":
+//                     alert(`${a} + ${b} = ${a + b}`)
+//                     break;        
+//                 case "-":
+//                     alert(`${a} - ${b} = ${a - b}`)
+//                     break;
+//                 case "*":
+//                     alert(`${a} * ${b} = ${a * b}`)
+//                     break;
+//                 case "/":
+//                     alert(`${a} / ${b} = ${a / b}`)
+//                     break;
+//                 case "^":
+//                     alert(`${a} ^ ${b} = ${a ** b}`)
+//                     break;
+//                 case "%":
+//                     alert(`${a} % ${b} = ${a % b}`)
+//                     break;
+//                 default:
+//                     alert('No no no please no')
+//                     break;
+//             }
+//             // if ( operate == "+" ) {
+//             //     alert(`${a} + ${b} = ${a + b}`)
+//             // }else if ( operate == "-") {
+//             //     alert(`${a} - ${b} = ${a - b}`)
+//             // }else if ( operate == "+") {
+//             //     alert(`${a} * ${b} = ${a * b}`)
+//             // } else if ( operate == "/") {
+//             //     alert(`${a} / ${b} = ${a / b}`)
+//             // } else if ( operate == "^") {
+//             //     alert(`${a} ** ${b} = ${a ** b}`)
+//             // } else if ( operate == "%") {
+//             //     alert(`${a} % ${b} = ${a % b}`)
+//             // }else{
+//             //     alert('NOOOOOO')
+//             // }
+//         }else{
+//             console.log('Ok, Maybe you will great')
+//         }
+//         break;
+// }
